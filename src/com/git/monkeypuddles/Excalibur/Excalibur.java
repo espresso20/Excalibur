09289fabaf8da5efadf8722e7b21ba1d8ffbc334
monkeypuddles <monkeyservers@gmail.com>
2013-01-23 18:52:39 -0800

    jar, yml, readme changes

--- src/com/git/monkeypuddles/Excalibur/Excalibur.java
+++ src/com/git/monkeypuddles/Excalibur/Excalibur.java
@@ -1,5 +1,20 @@
 package com.git.monkeypuddles.Excalibur;
 
-public class Excalibur {
+import java.util.logging.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public final class Excalibur extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		//TODO
+		getLogger() .info("onEnable has been invoked!");
+	}
+	
+	@Override
+	public void onDisable() {
+		//TODO
+		getLogger() .info("onDisable has been invoked!:);
+	}
 }
+
